58f65a1afb1551b103c373489615ac115ce6d13a
MKhodagholi <mdkhodagholi@gmail.com>
2023-01-24 13:52:58 +0330

    15. add type logo Replai & change logo replai

--- components/Layout/Nav.jsx
+++ components/Layout/Nav.jsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import replaiLogo from "../../public/assets/svg/replai_logo.svg";
+import replaiTypeLogo from "../../public/assets/svg/replai_type-logo.svg";
 
 export default function Nav() {
   return (
     <nav className="mt-[1.875rem] ml-[2rem] list-none md:ml-[6.25rem]">
       <li className="flex items-center">
-        <Image src={replaiLogo} alt="replai-logo" />
-        <h2 className="ml-2.5 font-Impact text-slightyBlack">
-          Repl<span className="text-blueColor font-Impact">ai</span>
-        </h2>
+        <Image src={replaiLogo} alt="replai-logo" className="mr-4" />
+        <Image src={replaiTypeLogo} alt="replai-type-logo" />
       </li>
     </nav>
   );
